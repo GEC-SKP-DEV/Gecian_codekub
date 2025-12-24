7055c8135b81dc7db4d6b50d0f28240a929dffc2
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-24 15:17:19 +0530

    feat(desktopIcon):make new icon list to apper

---
+++ install/desktop/set-desktopIcon.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Apps you want on the Desktop
+apps=(
+  "Alacritty.desktop"
+  "Neovim.desktop"
+  "pinta_pinta.desktop"
+  "Activity.desktop"
+  "Docker.desktop"
+  "Omakub.desktop"
+  "org.gnome.Nautilus.desktop"
+)
+
+# Where .desktop files live
+desktop_dirs=(
+  "/var/lib/flatpak/exports/share/applications"
+  "/usr/share/applications"
+  "/usr/local/share/applications"
+  "$HOME/.local/share/applications"
+)
+
+DESKTOP="$HOME/Desktop"
+mkdir -p "$DESKTOP"
+
+for app in "${apps[@]}"; do
+  for dir in "${desktop_dirs[@]}"; do
+    if [ -f "$dir/$app" ]; then
+      echo "Adding $app to Desktop"
+      cp "$dir/$app" "$DESKTOP/"
+      chmod +x "$DESKTOP/$app"
+      break
+    fi
+  done
+done
+
+echo "Done. If icons don't appear, enable Desktop Icons in GNOME."
