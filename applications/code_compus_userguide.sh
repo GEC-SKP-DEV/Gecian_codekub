fa920373737ede3cdb28ac26f91ecd4a960a1263
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-24 22:36:52 +0530

    trying to solve the other application inside folder not installting

---
+++ applications/code_compus_userguide.sh
@@ -0,0 +1,10 @@
+cat <<EOF >~/.local/share/applications/UserGuide.desktop
+[Desktop Entry]
+Name=User Guide
+Icon=codecompass
+Comment=Open the CodeCompass user guide
+Exec=xdg-open https://codecompasss.github.io/code_campus_userguide/
+Terminal=false
+Type=Application
+Categories=X-CodeCompass;
+EOF
