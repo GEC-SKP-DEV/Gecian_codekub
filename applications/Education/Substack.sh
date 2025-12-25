17086fd7fe8f5ace0820c6d10a6b15c883568b9c
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-25 17:35:49 +0530

    also added substack

---
+++ applications/Education/Substack.sh
@@ -0,0 +1,12 @@
+
+cat <<EOF > ~/.local/share/applications/Substack.desktop
+[Desktop Entry]
+Name=Substack
+Comment=Read and write newsletters
+Exec=xdg-open https://substack.com/
+Icon=/home/$USER/.local/share/omakub/applications/icons/mitocw.png
+Terminal=false
+Type=Application
+Categories=Education;News;
+StartupNotify=true
+EOF
